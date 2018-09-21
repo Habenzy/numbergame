5e6622531fa02c2bd129f5b0d6fbd2525cc107e7
Habenzy <robertrstauss@gmail.com>
2018-09-21 11:26:11 -0500

    initial working build

---
+++ number_game.js
@@ -0,0 +1,48 @@
+const readline = require('readline');
+
+const readlineInterface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function ask(questionText) {
+  return new Promise((resolve, reject) => {
+    readlineInterface.question(questionText, resolve);
+  });
+};
+
+let highGuess = 100;
+let lowGuess = 1;
+
+start();
+async function start() {
+const currentGuess = Math.floor(Math.random() * (highGuess - lowGuess + 1) + lowGuess);
+//ask 'is your number (guess) Y/N?' between two values.
+let answer = await ask('Is your number...' + currentGuess + ' Y/N? ')
+if (answer.toLowerCase() === 'y') {
+  console.log('I am a victorious computer!');
+  process.exit()
+}
+else if (answer.toLowerCase() === 'n') {
+  //modifies range according to user input (H/L)
+  let highLow = await ask('Is my number high or low (H/L)? ')
+  if (highLow.toLowerCase() === 'h') {
+    //if computer's number is high modify highGuess
+    highGuess = currentGuess;
+    start()
+  }
+  else if (highLow.toLowerCase() === 'l') {
+    //if computer's number is low modify lowGuess
+    lowGuess = currentGuess;
+    start()
+  }
+  else {console.log('Error. Invalid input. Goodbye.');
+process.exit()};
+}
+else {console.log('Error. Invalid input. Goodbye.')
+process.exit()};
+  };
+
+//to be implemented:
+  //cheat detector
+  //smarter guess algorithm
