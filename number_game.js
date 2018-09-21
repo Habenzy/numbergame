ce9d68f856fb48a525a871737443c5cc04833fd1
Habenzy <robertrstauss@gmail.com>
2018-09-21 13:09:10 -0500

    implemented cheat detection and no longer exits on invalid input

--- number_game.js
+++ number_game.js
@@ -17,32 +17,40 @@ let lowGuess = 1;
 start();
 async function start() {
 const currentGuess = Math.floor(Math.random() * (highGuess - lowGuess + 1) + lowGuess);
+  //cheat detector: if lowGuess >= highGuess; cheater cheater pumpkin eater
+if (lowGuess + 1 >= highGuess) {
+  console.log('CHEATER, CHEATER, CHEATER!!!!!!');
+  process.exit();
+}
+else {
 //ask 'is your number (guess) Y/N?' between two values.
-let answer = await ask('Is your number...' + currentGuess + ' Y/N? ')
-if (answer.toLowerCase() === 'y') {
-  console.log('I am a victorious computer!');
-  process.exit()
-}
-else if (answer.toLowerCase() === 'n') {
+  let answer = await ask('Is your number...' + currentGuess + ' Y/N? ')
+  if (answer.toLowerCase() === 'y') {
+    console.log('I am a victorious computer!');
+    process.exit()
+  }
+  else if (answer.toLowerCase() === 'n') {
   //modifies range according to user input (H/L)
-  let highLow = await ask('Is my number high or low (H/L)? ')
-  if (highLow.toLowerCase() === 'h') {
+    let highLow = await ask('Is my number high or low (H/L)? ')
+    if (highLow.toLowerCase() === 'h') {
     //if computer's number is high modify highGuess
-    highGuess = currentGuess;
-    start()
-  }
-  else if (highLow.toLowerCase() === 'l') {
+      highGuess = currentGuess;
+      start()
+    }
+    else if (highLow.toLowerCase() === 'l') {
     //if computer's number is low modify lowGuess
-    lowGuess = currentGuess;
-    start()
-  }
-  else {console.log('Error. Invalid input. Goodbye.');
-process.exit()};
+      lowGuess = currentGuess;
+      start()
+    }
+    else {
+      console.log('Error. Invalid input.');
+      start()};
 }
-else {console.log('Error. Invalid input. Goodbye.')
-process.exit()};
-  };
+  else {
+    console.log('Error. Invalid input.')
+    start()};
+  }};
 
 //to be implemented:
-  //cheat detector
+
   //smarter guess algorithm
